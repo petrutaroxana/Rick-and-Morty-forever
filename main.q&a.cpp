3d545854e671ae60384ddedc38f6832139b0c3e4
petrutaroxana <32909152+petrutaroxana@users.noreply.github.com>
2017-11-22 11:07:09 +0200

    Add files via upload

---
+++ main.q&a.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int selector;
+   cout<<"1. Romania has the most beautiful waterfall in the world. Which is it?"<<endl;
+   cout<<"1.Urlatoarea"<<endl;cout<<"2.Bigar"<<endl;cout<<"3.Valul Miresei"<<endl;cout<<"4.Balea"<<endl;
+   cin>>selector;
+       switch(selector)
+{
+    case 1: cout<<"Wrong answer! Bigar Waterfall was voted the most unique waterfall in the world"<<endl<<endl<<endl;break;
+    case 2: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer! Bigar Waterfall was voted the most unique waterfall in the world"<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer! Bigar Waterfall was voted the most unique waterfall in the world"<<endl<<endl<<endl;
+}
+   cout<<"2. The man who inspired Dracula's character is actually called Vlad Dracul III and is known in the Romanian History as Vlad Tepes.In which region of Romania was Dracula born?"<<endl;
+   cout<<"1. Transylvania"<<endl;cout<<"2. Vallachia"<<endl;cout<<"3. Moldavia"<<endl;cout<<"4. Dobruja"<<endl;
+   cin>>selector;
+       switch(selector)
+{
+    case 1: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer! Vlad Tepes was born in the city of Sighisoara in Transylvania as the son of Vlad Dracul II and a noble Transylvanian woman."<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer! Vlad Tepes was born in the city of Sighisoara in Transylvania as the son of Vlad Dracul II and a noble Transylvanian woman."<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer! Vlad Tepes was born in the city of Sighisoara in Transylvania as the son of Vlad Dracul II and a noble Transylvanian woman."<<endl<<endl<<endl;break;
+}
+   cout<<"3. Romania has one of the happiest cemeteries on Earth.What is the name of the little village where this place is tucked away?"<<endl;
+   cout<<"1. Campulung Muscel"<<endl;cout<<"2. Rimetea"<<endl;cout<<"3. Sapanta"<<endl;cout<<"4. Targu Secuiesc"<<endl;
+   cin>>selector;
+    switch(selector)
+{
+    case 1: cout<<"Wrong answer!The Merry Cemetery is placed in a tiny village of Maramures,Sapanta."<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer!The Merry Cemetery is placed in a tiny village of Maramures,Sapanta."<<endl<<endl<<endl;break;
+    case 3: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer!The Merry Cemetery is placed in a tiny village of Maramures,Sapanta."<<endl<<endl<<endl;break;
+}
+   cout<<"4. The Palace of Parliament, also known as the People's Palace, is the world's largest civilian building with an administrative function and the second-largest building in the world after the Pentagon in the US.In which city is placed this building?"<<endl;
+   cout<<"1. Braila"<<endl;cout<<"2. Sibiu"<<endl;cout<<"3. Bucharest"<<endl;cout<<"4. Iasi"<<endl;
+   cin>>selector;
+       switch(selector)
+{
+    case 1: cout<<"Wrong answer! The Palace of Parliament is placed in Bucharest,the capital city of Romania."<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer! The Palace of Parliament is placed in Bucharest,the capital city of Romania."<<endl<<endl<<endl;break;
+    case 3: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer! The Palace of Parliament is placed in Bucharest,the capital city of Romania."<<endl<<endl<<endl;break;
+}
+   cout<<"5. George Emil Palade was Romanian-American cell byologist.Described as ” the most influential cell biologist ever”,in 1974 he was awarded the Nobel Prize in Physiology and Medicine.Which was his most notable discovery in Medicine?"<<endl;
+   cout<<"1. the ribosomes"<<endl;cout<<"2. the insulin"<<endl;cout<<"3. the stethoscope"<<endl;cout<<"the chemotherapy"<<endl;
+   cin>>selector;
+      switch(selector)
+{
+   case 1: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+   case 2: cout<<"Wrong answer!His most notable discovery was the ribosomes of the endoplasmic reticulum – which he first described in 1955."<<endl<<endl<<endl;break;
+   case 3: cout<<"Wrong answer!His most notable discovery was the ribosomes of the endoplasmic reticulum – which he first described in 1955."<<endl<<endl<<endl;break;
+   case 4: cout<<"Wrong answer!His most notable discovery was the ribosomes of the endoplasmic reticulum – which he first described in 1955."<<endl<<endl<<endl;break;
+}
+   cout<<"6. Untold Festival is the largest annual electronic music festival held in Romania.In which city is it taking place?"<<endl;
+   cout<<"1. Iasi"<<endl;cout<<"2. Bucharest"<<endl;cout<<"3. Sibiu"<<endl;cout<<"4. Cluj-Napoca"<<endl;
+   cin>>selector;
+      switch(selector)
+{
+    case 1: cout<<"Wrong answer!Untold Festival is taking place in Cluj-Napoca at Central Park, with the main stage at Cluj Arena"<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer!Untold Festival is taking place in Cluj-Napoca at Central Park, with the main stage at Cluj Arena"<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer!Untold Festival is taking place in Cluj-Napoca at Central Park, with the main stage at Cluj Arena"<<endl<<endl<<endl;break;
+    case 4: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+}
+   cout<<"7. The first ever perfect 10 in the Olympic Games was given to a Romanian gymnast.What's her name?"<<endl;
+   cout<<"1. Nadia Comaneci"<<endl;cout<<"2. Elena Leusteanu"<<endl;cout<<"3. Sandra Izbasa"<<endl;cout<<"4. Laura Cutina"<<endl;
+   cin>>selector;
+       switch(selector)
+{
+    case 1: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer!The first ever first perfect 10 in the Olympic Games was given to Nadia Comaneci!"<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer!The first ever first perfect 10 in the Olympic Games was given to Nadia Comaneci!"<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer!The first ever first perfect 10 in the Olympic Games was given to Nadia Comaneci!"<<endl<<endl<<endl;break;
+}
+   cout<<"8. Romania was the only communist country in the Warsaw Pact to violently put an end to the country's communist reign and overthrew the government with force.In what year took place the revolution?"<<endl;
+   cout<<"1. 1948"<<endl;cout<<"2. 1989"<<endl;cout<<"3. 1983"<<endl;cout<<"4. 1918"<<endl;
+   cin>>selector;
+       switch(selector)
+{
+    case 1: cout<<"Wrong answer!The Romanian Revolution took place in 1989 and was ultimately culminating in the show trial and execution of longtime Communist leader Nicolae Ceausescu and his wife Elena."<<endl<<endl<<endl;break;
+    case 2: cout<<"Right answer!You got 1 point"<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer!The Romanian Revolution took place in 1989 and was ultimately culminating in the show trial and execution of longtime Communist leader Nicolae Ceausescu and his wife Elena."<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer!The Romanian Revolution took place in 1989 and was ultimately culminating in the show trial and execution of longtime Communist leader Nicolae Ceausescu and his wife Elena."<<endl<<endl<<endl;break;
+}
+   cout<<"9. Who is considered the greatest Romanian footballer of all time?"<<endl;
+   cout<<"1. Gheorghe Hagi"<<endl;cout<<"2. Helmuth Duckadam"<<endl;cout<<"3. Adrian Mutu"<<endl;cout<<"4. Nicolae Dobrin"<<endl;
+   cin>>selector;
+        switch(selector)
+{
+    case 1: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer!Nicknamed ''The Maradona of the Carpathians'', Hagi is considered a hero in his homeland."<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer!Nicknamed ''The Maradona of the Carpathians'', Hagi is considered a hero in his homeland."<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer!Nicknamed ''The Maradona of the Carpathians'', Hagi is considered a hero in his homeland."<<endl<<endl<<endl;break;
+}
+   cout<<"10. The Romanian Sphinx is a rock formation in the Carpatians,in the ...."<<endl;
+   cout<<"1. Bucegi Mountains"<<endl;cout<<"2. Fagaras Mountains"<<endl;cout<<"3. Parang Mountains"<<endl;cout<<"4. Retezat Mountains"<<endl;
+   cin>>selector;
+        switch(selector)
+{
+    case 1: cout<<"Right answer!You got 1 point!"<<endl<<endl<<endl;break;
+    case 2: cout<<"Wrong answer!The Romanian Sphinx is a rock formation in the Bucegi Mountains."<<endl<<endl<<endl;break;
+    case 3: cout<<"Wrong answer!The Romanian Sphinx is a rock formation in the Bucegi Mountains."<<endl<<endl<<endl;break;
+    case 4: cout<<"Wrong answer!The Romanian Sphinx is a rock formation in the Bucegi Mountains."<<endl<<endl<<endl;break;
+}
+}
